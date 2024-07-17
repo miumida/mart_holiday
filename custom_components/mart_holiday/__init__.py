397b78f8d0d2dd0baab31b4249eca27cb682a541
miumida <46196137+miumida@users.noreply.github.com>
2024-07-17 15:54:56 +0900

    Update __init__.py

--- custom_components/mart_holiday/__init__.py
+++ custom_components/mart_holiday/__init__.py
@@ -29,9 +29,7 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Mart Holiday from a config entry."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, PLATFORM)
-    )
+    await hass.config_entries.async_forward_entry_setup(entry, PLATFORM)
 
     return True
 
